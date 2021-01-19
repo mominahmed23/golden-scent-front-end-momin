2487687f4b481844135aeec9b49ffef9758c09e3
mominahmed23 <mominahmad324@gmail.com>
2021-01-19 18:03:43 +0500

    Card Component UI part done that includes styling with dummy text

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="product-card">
-    <p>This is a product of perfume</p>
-      <b-button variant="success">Button</b-button>
+      <div class="custom-badge"><span>Exclusive</span></div>
+    <div class="display-image" style="background-image:url('https://pngimg.com/uploads/perfume/perfume_PNG10262.png')"></div>
+    <h1 class="name">5th Avenue</h1>
+    <h3 class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit officiis, magni velit, a, volup!</h3>
+
+     <div class="pricing pt-2">
+         <div class="d-flex justify-content-between mb-2">
+             <p class="price">SAR 120</p>
+             <p class="discount-percentage">-20%</p>
+         </div>
+         <div class="d-flex justify-content-between">
+             <p class="discarded-amount"><del>SAR 150</del></p>
+             <p class="saved-amount">Save SAR 30</p>
+         </div>
+     </div>
   </div>
 </template>
 
@@ -17,9 +30,60 @@ export default {
 
 <style lang="scss">
 .product-card{
-    p {
-  color: rgb(251, 255, 22);
-}
+    background-color: #eeeeee;
+    padding: 25px;
+    .custom-badge{
+        span{
+            text-transform: uppercase;
+            font-size: 14px;
+            font-weight: bold;
+            border: 2px solid black;
+            border-radius: 7px;
+            padding: 2px 4px;
+        }
+    }
+    .display-image{
+        height: 320px;
+        margin-bottom: 20px;
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
+    .name{
+        font-size: 18px;
+        font-weight: bold;
+    }
+    .description{
+        font-size: 14px;
+        color: #7b7b7b;
+        font-weight: 600;
+    }
+    .pricing{
+        p{
+            margin: 0;
+        }
+        .price{
+            font-size: 18px;
+            font-weight: bold;
+            color: red;
+        }
+        .discount-percentage{
+            font-size: 14px;
+            font-weight: 600;
+            background-color: #b3d185;
+            border-radius: 4px;
+            padding: 4px 5px;
+        }
+        .discarded-amount{
+            font-size: 14px;
+            font-weight: 500;
+        }
+        .saved-amount{
+            font-size: 14px;
+            font-weight: 600;
+        }
+    }
+
 }
 
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <h1>Products Page</h1>
-    <product-card />
+  <div class="products-page">
+    <h1 class="text-center">Golden Scent</h1>
+    <h2 class="text-center ">Products</h2>
+    <b-container class="pt-4">
+  <b-row>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+    <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4"> <productCard /></b-col>
+   
+  </b-row>
+
+</b-container>
+   
   </div>
 </template>
 
@@ -10,15 +23,16 @@ import ProductCard from './ProductCard.vue'
 export default {
   components: { ProductCard },
   name: 'Products',
-  props: {
-    msg: String
-  }
 }
 </script>
 
 
-<style>
-h1 {
-  color: blueviolet;
+<style lang="scss">
+.products-page{
+  padding-top: 50px;
+   h2{
+    color: #faa7b5;
+  }
+ 
 }
 </style>
