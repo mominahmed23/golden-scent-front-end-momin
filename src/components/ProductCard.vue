53360bb47dd26061bfe2542bb908852cd543a66c
mominahmed23 <mominahmad324@gmail.com>
2021-01-19 22:08:16 +0500

    Binded data and cards made dynamic. Some minor styling remaining

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,91 +1,117 @@
 <template>
   <div class="product-card">
-      <div class="custom-badge"><span>Exclusive</span></div>
-    <div class="display-image" :style="{backgroundImage: `url(https:${hit.image_url}`}"></div>
-    <h1 class="name">{{hit.name}}</h1>
-    <h3 class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit officiis, magni velit, a, volup!</h3>
+    <div class="custom-badge"><span>Exclusive</span></div>
+    <div
+      class="display-image"
+      :style="{ backgroundImage: `url(https:${hit.image_url}` }"
+    ></div>
+    <h1 class="name">{{ hit.short_name }}</h1>
+    <h3 class="description" v-html="hit.description"></h3>
 
-     <div class="pricing pt-2">
-         <div class="d-flex justify-content-between mb-2">
-             <p class="price">{{hit.currency}} {{hit.special_price}}</p>
-             <p class="discount-percentage">-20%</p>
-         </div>
-         <div class="d-flex justify-content-between">
-             <p class="discarded-amount"><del>{{hit.currency}} 150</del></p>
-             <p class="saved-amount">Save {{hit.currency}} 30</p>
-         </div>
-     </div>
+    <div class="pricing pt-2">
+      <div class="d-flex justify-content-between mb-2">
+        <p class="price">{{ hit.currency }} {{ price.default }}</p>
+        <p v-if="!!getDiscountPercentage()" class="discount-percentage">
+          -{{ getDiscountPercentage() }}%
+        </p>
+      </div>
+      <div
+        v-if="!!getDiscountPercentage()"
+        class="d-flex justify-content-between"
+      >
+        <p class="discarded-amount">
+          <del>{{ hit.currency }} {{ price.default_original }}</del>
+        </p>
+        <p class="saved-amount">
+          Save {{ hit.currency }} {{ price.default_original - price.default }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductCard',
+  name: "ProductCard",
   props: ["hit"],
-     mounted () {
-      console.log(this.hit);
-  }
-}
+  mounted() {
+    console.log(this.price, "price");
+  },
+  data() {
+    return {
+      price: this.hit.price[this.hit.currency],
+    };
+  },
+  methods: {
+    getDiscountPercentage() {
+      const {
+        default: discountedPrice,
+        default_original: originalPrice,
+      } = this.price;
+      return Math.floor(
+        ((originalPrice - discountedPrice) / originalPrice) * 100
+      );
+    },
+  },
+};
 </script>
 
 
 <style lang="scss">
-.product-card{
-    height: 100%;
-    background-color: #eeeeee;
-    padding: 25px;
-    .custom-badge{
-        span{
-            text-transform: uppercase;
-            font-size: 14px;
-            font-weight: bold;
-            border: 2px solid black;
-            border-radius: 7px;
-            padding: 2px 4px;
-        }
+.product-card {
+  height: 100%;
+  background-color: #eeeeee;
+  padding: 25px;
+  .custom-badge {
+    span {
+      text-transform: uppercase;
+      font-size: 14px;
+      font-weight: bold;
+      border: 2px solid black;
+      border-radius: 7px;
+      padding: 2px 4px;
     }
-    .display-image{
-        height: 320px;
-        margin-bottom: 20px;
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: contain;
+  }
+  .display-image {
+    height: 320px;
+    margin-bottom: 15px;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+  .name {
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .description {
+    font-size: 14px;
+    color: #7b7b7b;
+    font-weight: 600;
+  }
+  .pricing {
+    p {
+      margin: 0;
     }
-    .name{
-        font-size: 18px;
-        font-weight: bold;
+    .price {
+      font-size: 18px;
+      font-weight: bold;
+      color: red;
     }
-    .description{
-        font-size: 14px;
-        color: #7b7b7b;
-        font-weight: 600;
+    .discount-percentage {
+      font-size: 14px;
+      font-weight: 600;
+      background-color: #b3d185;
+      border-radius: 4px;
+      padding: 4px 5px;
     }
-    .pricing{
-        p{
-            margin: 0;
-        }
-        .price{
-            font-size: 18px;
-            font-weight: bold;
-            color: red;
-        }
-        .discount-percentage{
-            font-size: 14px;
-            font-weight: 600;
-            background-color: #b3d185;
-            border-radius: 4px;
-            padding: 4px 5px;
-        }
-        .discarded-amount{
-            font-size: 14px;
-            font-weight: 500;
-        }
-        .saved-amount{
-            font-size: 14px;
-            font-weight: 600;
-        }
+    .discarded-amount {
+      font-size: 14px;
+      font-weight: 500;
     }
-
+    .saved-amount {
+      font-size: 14px;
+      font-weight: 600;
+    }
+  }
 }
-
-</style>
+</style>a   
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,56 +1,87 @@
 <template>
   <div class="products-page">
     <h1 class="text-center">Golden Scent</h1>
-    <h2 class="text-center ">Products</h2>
+    <h2 class="text-center products">Products</h2>
     <b-container class="pt-4">
       <div class="text-center pt-4" v-if="loading">
-      <b-spinner style="width: 4rem; height: 4rem;"  variant="primary" type="grow" label="Spinning"></b-spinner>
+        <b-spinner
+          style="width: 4rem; height: 4rem"
+          variant="primary"
+          type="grow"
+          label="Spinning"
+        ></b-spinner>
       </div>
-      <b-row v-if="hits">
-        <b-col cols="12" sm="6" md="6" lg="4" xl="3" class="mb-4" v-for="hit in hits" :key="hit.objectID"> <productCard :hit="hit" /></b-col>
+      <div class="text-center pt-5" v-if="errored">
+        <h2 class="text-center mt-3 text-warning">
+          Something Went Wrong! Try Again
+        </h2>
+        <b-button variant="outline-primary" v-on:click="fetchData"
+          >Try Again</b-button
+        >
+      </div>
+      <b-row v-if="hits && hits.length">
+        <b-col
+          cols="12"
+          sm="6"
+          md="6"
+          lg="4"
+          xl="3"
+          class="mb-4"
+          v-for="hit in hits"
+          :key="hit.objectID"
+        >
+          <productCard :hit="hit"
+        /></b-col>
       </b-row>
-
-</b-container>
-   
+      <h2 v-else class="text-center">No Products</h2>
+    </b-container>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import ProductCard from './ProductCard.vue'
+import axios from "axios";
+import ProductCard from "./ProductCard.vue";
 export default {
   components: { ProductCard },
-  name: 'Products',
-    data () {
+  name: "Products",
+  data() {
     return {
       hits: null,
       loading: true,
-      errored: false
-    }
+      errored: false,
+    };
   },
-    mounted () {
-    axios
-      .get('https://gs-euw1-public-data-prod.s3-eu-west-1.amazonaws.com/new-web/test/test.json')
-      .then(response => {
-        console.log(response.data);
-        this.hits = response.data.hits
-      })
-      .catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
-  }
-}
+  methods: {
+    fetchData() {
+      this.loading = true;
+      this.errored = false;
+      axios
+        .get(
+          "https://gs-euw1-public-data-prod.s3-eu-west-1.amazonaws.com/new-web/test/test.json"
+        )
+        .then((response) => {
+          this.loading = false;
+          this.errored = false;
+          this.hits = response.data.hits;
+        })
+        .catch((error) => {
+          this.errored = true;
+          this.loading = false;
+        });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
 </script>
 
 
 <style lang="scss">
-.products-page{
+.products-page {
   padding-top: 50px;
-   h2{
+  .products {
     color: #faa7b5;
   }
- 
 }
 </style>
